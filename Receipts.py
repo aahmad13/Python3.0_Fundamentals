522caf7deaf502eb85d43a355cddfe2bdf60225d
aahmad13 <62896776+aahmad13@users.noreply.github.com>
2020-03-30 15:20:26 -0400

    Update Receipts.py

--- Receipts.py
+++ Receipts.py
@@ -1,4 +1,3 @@
-
 lovely_loveseat_description='Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.'
 lovely_loveseat_price=254.00
 stylish_settee_description="Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
@@ -18,9 +17,3 @@ print("Customer One Items:")
 print("customer_one_itemization")
 print("Customer One Total:")
 print(customer_one_total)
-
-
-
-
-
-
